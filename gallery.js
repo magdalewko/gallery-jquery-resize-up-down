6e3f2f7066db76da810677c8a7723b2c8f95c9c8
Magda Lewko <mlewko@hotmail.co.uk>
2017-04-25 00:39:43 +0100

    project image

---
+++ gallery.js
@@ -0,0 +1,21 @@
+$("ul#gallery li").click(function(){
+	var dataTypeClicked = $(this).attr("data-room");
+	
+	$("img").each(function(index,value){
+		var dataType = $(this).attr("data-room-type");
+		if(dataTypeClicked != dataType){
+
+			$(this).animate({height:0},400);
+		}
+		
+		if(dataTypeClicked == dataType){
+			
+			$(this).css("height","400px");
+		}
+	});
+	
+});
+		
+		
+
+	
